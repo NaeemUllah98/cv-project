d967e4abacca707b83fd0d78ad3eda38e12e6fa7
Naeem Ullah98 <naeem66241@gmail.com>
2022-09-02 11:18:55 +0500

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container text-base  ">
+        <div className="flex">
+          <div className="w-2/5 sm: mt-2, sm: pt-2, sm:pl-2, sm: pr-2, sm: mt-4, md: pt-4, md:pl-4, md: pr-4, mt-6 pt-10 pl-8 ml-6 pr-8 border-r-2 border-slate-700">
+            <h1 className="text-xl text-black font-bold">
+              Naeem Ullah (React.js)
+            </h1>
+            <h1 className="text-black text-xl sm:pt-2, md:pt-4 lg: pt-6 font-bold">
+              About
+            </h1>
+            <p className="text-black">
+             <strong> I would like to propose my candidacy for your development team. I
+              am enthusiastically applying for this post because I firmly
+              believe that with my practical experience, I can become a good
+              performer at your company. My long-term career goal is to continue
+              to learn and grow my specialist skills and abilties.</strong>
+            </p>
+            <h2 className="text-black pt-12 pl-2 ml-4 font-bold">
+              Contact:
+            </h2>
+            <p className="pl-12 ml-8 font-bold">+923160729817</p>
+            <p className="pl-12 ml-8 font-bold">03480116624</p>
+            <h2 className="text-black pl-2 ml-4 font-bold">Email:</h2>
+            <a className="pl-12 ml-8 text-blue-600 font-bold" href="naeem66241@gmail.com" alt="gmail" target={"blank"}>naeem66241@gmail.com</a>
+            <h2 className="text-black pl-2 ml-4 font-bold">GitHub:</h2>
+            <a className="pl-12 ml-8 font-bold" href="https:/github.com/NaeemUllah98" alt="Github" target={"blank"}>
+              https:/github.com/NaeemUllah98
+            </a>
+            <h2 className="text-black  pl-2 ml-4 font-bold">Linkedin:</h2>
+            <a className="pl-12 ml-8 font-bold" href="linkedin.com/in/naeem-ullah" alt="linkedin" target={"blank"}>
+              linked.com/in/naeem-ullah6624
+            </a>
+          </div>
+          <div className="w-3/5 mt-6 pt-10 ml-6 pl-8 pr-8 text-black">
+            <h1 className="text-black text-xl font-bold">EXPERIENCE</h1>
+            <p className=" font-bold   whitespace-pre">Devionad.inc                                                                                       02/2022 to 08/2022</p>
+            <h2 className="    font-bold">Frontend Developer</h2>
+            <li className="pl-12 ml-8  ">
+            
+              6 Months experience as Frontend JS Developer in React.js and
+              Next.js.
+            </li>
+            <li className="pl-12 ml-8  ">
+            
+              Developed a lot of Responsive and fully SEO optimized websites and
+              apppcations.
+            </li>
+            <li className="pl-12 ml-8  ">
+
+              Developed Multiple Projects as Team Member in a cross-functional
+              team.
+            </li>
+            <h2 className="text-black font-bold  ">Pixiders.inc (Remote Based Canadian Company)</h2>
+            <li className="pl-12 ml-8  "> 3-Months internship </li>
+            <h1 className="text-black text-xl font-bold  ">EDUCATION</h1>
+            <p className="whitespace-pre font-bold pl-12 ml-8  ">Government College University Faisalabad                                          09/2019 to 08/2021</p>
+            <h3 className="text-black font-bold pl-12 ml-8  ">
+              M.sc Computer Science
+            </h3>
+            <p className="pl-12 ml-8  ">Obtained 3.60/4.0 CGPA</p>
+            <h1 className="text-black text-xl font-bold  ">SKILLS</h1>
+            
+              <li  className="text-black list-inside pl-12   ml-8"> <strong>Programming Languages:</strong>JavaScript ES6, Typescript, C++, C, Python.</li>
+              <li className="text-black list-inside pl-12   ml-8 whitespace-pre"><strong>Developmental Tools:</strong>Microsoft Visual Studio, Git, GitHub, Netlify, Npm,Yarn,
+              <br></br>     Sublime, Unity 3D, Dev C++, MySQL, Chrome & Developer Tools.</li>
+              <li className="text-black pl-12 list-inside whitespace-pre   ml-8"><strong>Frameworks and libraries:</strong> Next.js, React.js, Redux, Axios, React Router,<br></br>     Material UI, Tailwind CSS, BootStrap5.</li>
+              <li className="text-black pl-12   ml-8 "><strong>Markup Languages and Styling:</strong>HTML5, XHTML, XML, CSS3, SASS/SCSS.</li>
+              <li className="text-black pl-12   ml-8"> <strong>Web Scrapping:</strong>Beautiful Soap using python, Octoparse, Selenium.</li>
+              <li className="text-black pl-12   ml-8"> <strong>DB language:</strong>SQL</li>
+              <li className="text-black pl-12   ml-8"> <strong>CMS:</strong>Sanity.io, WordPress.</li>
+            <h1 className="text-black text-xl   font-bold">PROJECTS</h1>
+            <li className="text-black font-bold   pl-12 ml-8">
+              Personal Portfolio and Blog Website
+            </li>
+            <p className="pl-12 ml-8  ">
+              Using Next.js, React.js and Tailwind Css this website contains my
+              portfolio.
+            </p>
+            <p className="pl-12 ml-8 font-bold  ">
+              <a className="text-blue-600  " href="https://portfolioowebsite.netlify.app/"  alt="Portfolio" target={"blank"}> https://portfolioowebsite.netlify.app/
+              </a>
+            </p>
+            <li className="text-black pl-12 ml-8 font-bold  "> Spotify Clone</li>
+            <p className="pl-12 ml-8  ">
+              It is a clone of famous music player Spotify. Anyone can listen
+              his favorite songs. This clone is made using pure JavaScript, CSS3
+              and HTML5.
+            </p>
+            <li className="text-black pl-12 ml-8   font-bold">
+              Super Weather Forecastor
+            </li>
+            <p className="pl-12 ml-8  ">
+              This app can forecast the current weather using OpenWeatherMap
+              API. It can fetch weather details of almost all countries of the
+              world
+            </p>
+            <p className="pl-12 ml-8 font-bold  ">
+              <a className="text-blue-600  " href="https://super-weather-forecaster.netlify.app/" alt="weather App" target={"blank"} >https://super-weather-forecaster.netlify.app/ </a> 
+            </p>
+            <p className="pl-12 ml-8  ">Still continue...</p>
+            <h1 className="text-black text-xl   font-bold">Additional</h1>
+            <h2 className="text-black pl-12 ml-8  "><strong>Soft Skills:</strong>Active Listener, Problem Solver, Confident, Helpful Team Member.</h2>
+            <p className="text-black pl-12 ml-8  ">
+              <strong>Technical Skills:</strong> Googling, SDLC, Microsoft Project, Google Docs.
+            </p>
+          
+            
+            <p className="text-black pl-12 ml-8  "><strong>Languages: </strong>Fluent in English, Urdu, Punjabi and Saraiki.</p>
+        
+            <p className="text-black pl-12 ml-8  "><strong>Certificates:</strong> React.js udemy certificate, Responsive Web Design FreeCodeCamp
+              Certificate, Hacker Rank JavaScript Programming.</p>
+            
+            <h1 className="text-black text-xl font-bold  ">Activities</h1>
+            <li className="pl-12 ml-8  ">
+              Reading new Technologic Books and Blogs.
+            </li>
+            <li className="pl-12 ml-8   ">
+              Learning new skills and technology related to Web.
+            </li>
+            <li className="pl-12 ml-8">
+              Fond of Saraiki and Urdu Poetry and Music in leisure time.
+            </li><br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
